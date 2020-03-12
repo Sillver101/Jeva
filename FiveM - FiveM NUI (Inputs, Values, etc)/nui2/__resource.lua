c8230ecfc3a9fa97cb4e57ae7c76b6710e7e6d1a
Darryn Lloyd <darryn.lloyd@gmail.com>
2020-03-12 13:38:51 +0200

    Commit #00009
    Changed interface to be interactive

--- FiveM - FiveM NUI (Inputs, Values, etc)/nui2/__resource.lua
+++ FiveM - FiveM NUI (Inputs, Values, etc)/nui2/__resource.lua
@@ -2,10 +2,25 @@ resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 client_script "nui.lua"
 
-ui_page "html/index.html"
+ui_page "ui/index.html"
 files {
-    'html/index.html',
-    'html/index.js',
-    'html/index.css',
-    'html/reset.css'
+    'ui/index.html',
+    'ui/index.js',
+    'ui/index.css',
+    'ui/reset.css'
+    "html/10-codes.html"
+    "html/index.html"
+    "html/laws.html"
+    "html/License-Query.html"
+    "html/License-Result.php"
+    "html/sql_conn.php"
+    "html/styles.css"
+    "html/Vehicle-Result.php"
+    "html/Veh-Reg-Check.html"
+    "html/src/10-codes.pdf"
+    "html/src/bg.jpg"
+    "html/src/dmv.png"
+    "html/src/lspdlic.png"
+    "html/src/lspdps.png"
+    "html/src/sapd.png"
 }
